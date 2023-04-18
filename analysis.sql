ebe9f77e8333725245f331d449610a5994213808
Vishal Bagal <125578339+vsbagal@users.noreply.github.com>
2023-04-18 20:05:25 +0530

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -3,3 +3,7 @@ SELECT s.userid, sum(p.price) as total_amount
 FROM sales s INNER JOIN product p
 ON s.product_id = p.product_id
 GROUP BY s.userid;
+
+-- QUE2. How many days has each customer visited zomato?
+SELECT userid, count(distinct created_date) distinct_days
+FROM sales GROUP BY userid;
