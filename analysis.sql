b8a19caf22e12e4b8993ccb948273e9a84b89dc2
Vishal Bagal <125578339+vsbagal@users.noreply.github.com>
2023-04-18 20:50:31 +0530

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,4 +1,4 @@
--- QUE1. What is the total amount each customer spent on zomato?
+# QUE1. What is the total amount each customer spent on zomato?
 SELECT s.userid, sum(p.price) as total_amount
 FROM sales s INNER JOIN product p
 ON s.product_id = p.product_id
